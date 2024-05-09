51492719aec23ddade27ff329a9686c82498d38d
Muhammad Mahmood <mmuhammadabba@gmail.com>
2024-05-09 16:45:09 +0100

    feat: installed and config chakraUI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,26 @@
-import './App.css';
 import Home from './pages/dashboard/dashboard';
+import { Box, Text } from '@chakra-ui/react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from './components/footer/footer';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Box h="100dvh">
+      <Box>
+        {/* Header */}
+        <Text>Nav Bar</Text>
+      </Box>
+      <Box>
+        {/* Content */}
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </Box>
+
+      <Footer />
+    </Box>
   );
 }
 
---
+++ client/src/theme.ts
@@ -0,0 +1,52 @@
+import { extendTheme, defineStyle, defineStyleConfig } from '@chakra-ui/react';
+
+const PRIMARY = '#1D222B';
+const SECONDARY = '#285E61';
+
+const primary = defineStyle({
+  background: PRIMARY,
+  color: 'white',
+  _hover: {
+    background: '#0987A0',
+    outline: 'none',
+    borderColor: 'transparent',
+  },
+});
+const secondary = defineStyle({
+  background: SECONDARY,
+  color: 'white',
+  _hover: {
+    borderColor: 'transparent',
+    background: '#0BC5EA',
+    outline: 'none',
+  },
+});
+
+const outline = defineStyle({
+  background: 'transparent',
+  border: '1px solid',
+  color: PRIMARY,
+  _hover: {
+    boxShadow: 'md',
+    borderColor: PRIMARY,
+    // transform: 'scale(1.05)',
+  },
+});
+
+const ButtonTheme = defineStyleConfig({
+  variants: {
+    primary,
+    secondary,
+    outline,
+  },
+});
+
+export const theme = extendTheme({
+  colors: {
+    primary: PRIMARY,
+    secondary: SECONDARY,
+  },
+  components: {
+    Button: ButtonTheme,
+  },
+});
